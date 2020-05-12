799b2ff0976ed91586a1262dcbb9667fe56a530f
chris <chris.cave@abzu.ai>
2020-05-12 16:43:07 +0200

    adding requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup, find_packages
 
 # Layout & format inspired from https://github.com/navdeep-G/samplemod/blob/master/setup.py
 
+
+
 with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
+
+requirements = ['feyn == 1.1.*']
 
 setup(
     name='feynplots',
@@ -17,5 +21,6 @@ setup(
     author_email='chris.cave@abzu.ai',
     url='https://github.com/chriscave/cave-plots',
     license=license,
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=requirements
 )
