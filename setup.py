c20696c5c434a4ea78c0801e3cf1a095cc7ff524
chriscave <chriscave89@gmail.com>
2020-05-20 15:56:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = ['feyn == 1.1.*', 'numpy >= 1.18.1', 'matplotlib >= 3.1.3']
 
 setup(
     name='feynplots',
-    version='1.0.3',
+    version='1.0.4',
     description='Plotting Feyn graphs',
     long_description='Feynplots is a package to visualise each interaction in a Feyn Graph. Find the [documentation here](https://github.com/chriscave/cave-plots). See [here for more on starting with Feyn](https://docs.abzu.ai/docs/guides/quick_start.html) and [understanding the QLattice.](https://docs.abzu.ai/docs/guides/qlattice.html)',
     long_description_content_type='text/markdown',
